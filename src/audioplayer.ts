0c930e987e936c4f25e85099578e18040555347b
Nathan Graule <solarliner@gmail.com>
2018-02-12 22:19:02 +0100

    Added renderer optional argument on AudioPlayer

--- src/audioplayer.ts
+++ src/audioplayer.ts
@@ -1,15 +1,28 @@
+import { IRenderer } from "./abstract/renderer";
+
 export class AudioPlayer {
     private player: HTMLAudioElement;
     private element: HTMLElement;
+    private renderer?: IRenderer;
 
     get paused() {
         return this.player.paused;
     }
 
-    constructor(id: string, file: string) {
+    constructor(id: string, file: string, renderer?: IRenderer) {
         this.element = document.getElementById(id);
         this.element.innerText = "Audio player.";
         this.player = new Audio(file);
+
+        this.renderer = renderer;
+    }
+
+    public setRenderer(renderer: IRenderer) {
+        if(renderer == this.renderer)
+            return;
+        
+        this.renderer.dispose();
+        this.renderer = renderer;
     }
 
     public play() {
