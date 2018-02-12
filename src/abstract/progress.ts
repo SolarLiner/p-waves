a9cb1b061c2e3563599ea4605b47403a2f70541f
Nathan Graule <solarliner@gmail.com>
2018-02-12 22:01:05 +0100

    Moved BaseProgress to abstract/renderer

--- src/abstract/progress.ts
+++
@@ -1,8 +0,0 @@
-import { IRenderer } from "./renderer";
-
-export abstract class BaseProgress implements IRenderer {
-    public progress: number;
-    animationFrameReference: number;
-
-    abstract render(ms: number): void;
-}
--- src/abstract/renderer.ts
+++ src/abstract/renderer.ts
@@ -2,3 +2,10 @@ export interface IRenderer {
     animationFrameReference: number;
     render(ms: number): void;
 }
+
+export abstract class BaseProgress implements IRenderer {
+    public progress: number;
+    animationFrameReference: number;
+
+    abstract render(ms: number): void;
+}
