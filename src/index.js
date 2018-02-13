a29121ccacc6e3d02860fbad53ab67a206db3779
Nathan Graule <solarliner@gmail.com>
2018-02-13 23:40:24 +0100

    Now testing bootstrap compound renderer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 let AudioPlayer = require('./audioplayer');
 let Renderers = require('./renderers/index');
+let Layouts = require('./layouts/index');
 window.AudioPlayer = AudioPlayer.AudioPlayer;
 
-let player = new AudioPlayer.AudioPlayer('player', 'file.mp3');
-let render = new Renderers.TimestampRenderer(player.element, player.player);
+let player = new AudioPlayer.AudioPlayer('file.mp3');
+let root = document.getElementById('player');
+
+let renderers = [
+    new Renderers.ProgressbarRenderer(root, player.player),
+    new Renderers.TimestampRenderer(root, player.player)
+];
+
+let render = new Layouts.BootstrapGridRendererGroup(root, player.player, renderers);
 player.setRenderer(render);
 
 let button = document.getElementById('play-button');
