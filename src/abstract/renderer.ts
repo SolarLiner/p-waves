7b6fc7be0e093912d0ca5485a4e5db3734addc0d
Nathan Graule <solarliner@gmail.com>
2018-02-14 00:08:41 +0100

    Adding base setRoot() to change root element (for compound renderers)

--- src/abstract/renderer.ts
+++ src/abstract/renderer.ts
@@ -11,6 +11,11 @@ export abstract class BaseRenderer implements IRenderer {
 
     constructor(protected root: HTMLElement, protected audioRef: HTMLAudioElement) { }
 
+    public setRoot(newRoot: HTMLElement) {
+        newRoot.innerHTML = this.root.innerHTML;
+        this.root = newRoot;
+    }
+
     abstract render(ms: number): void;
     abstract dispose(): void;
 }
