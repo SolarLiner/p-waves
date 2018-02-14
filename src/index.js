d9a889135178676dbaa7161633afe9bcfd1f6799
Nathan Graule <solarliner@gmail.com>
2018-02-14 13:12:49 +0100

    Index.js using module.js which injects module into window.AudioPlayer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ let player = new AudioPlayer.Player('file.mp3');
 let root = document.getElementById('player');
 
 let renderers = [
-    new Renderers.PlayPauseButtonRenderer(root, player.player),
-    new Renderers.ProgressbarRenderer(root, player.player),
-    new Renderers.TimestampRenderer(root, player.player)
+    new AudioPlayer.Renderers.PlayPauseButtonRenderer(root, player.player),
+    new AudioPlayer.Renderers.ProgressbarRenderer(root, player.player),
+    new AudioPlayer.Renderers.TimestampRenderer(root, player.player)
 ];
 
-let render = new Layouts.BootstrapGridRendererGroup(root, player.player, renderers);
+let render = new AudioPlayer.Layouts.BootstrapGridRendererGroup(root, player.player, renderers);
 player.setRenderer(render);
