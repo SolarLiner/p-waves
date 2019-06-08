d1deeda4191b2dc963d14937a3acd5f9770f28d1
Nathan Graule <solarliner@gmail;com>
2019-06-08 22:18:39 +0200

    Remove unnecessary imports

--- src/preact/index.tsx
+++ src/preact/index.tsx
@@ -1,8 +1,6 @@
 import { h, RenderableProps } from "preact";
-import { useContext, useState } from "preact/hooks";
 
 import { getContext, useAudioPlayer } from "./context";
-import { AudioPlayer } from "../audioplayer";
 
 export function Provider({ children }: RenderableProps<{}>) {
   const ctx = getContext();
