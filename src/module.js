94eda167c1270e06217afdfcdcf3d530eb3c084e
Nathan Graule <solarliner@gmail;com>
2019-06-08 22:18:50 +0200

    Re-export modules for microbundle to use

--- src/module.js
+++
@@ -1,9 +0,0 @@
-var AudioPlayer = require('./audioplayer');
-var Renderers = require('./renderers/index');
-var Layouts = require('./layouts/index');
-
-window.AudioPlayer = {
-    Player: AudioPlayer.AudioPlayer,
-    Renderers: Renderers,
-    Layouts: Layouts
-};
